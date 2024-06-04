f376e7e1b66c876420b6426aeea1ce2c956793d8
Oskar Fraser-Krauss <oskarfkrauss@gmail.com>
2024-06-04 14:30:07 +0100

    correct cloud run typo

--- scrape_build_deploy.sh
+++ scrape_build_deploy.sh
@@ -10,7 +10,7 @@ docker build -t gcr.io/phd-compute-400214/web-container /home/oskar/code/web_con
 # push docker image to google container registry
 docker push gcr.io/phd-compute-400214/web-container
 # deploy to cloud run
-gcloud run deploy web-contianer --image gcr.io/phd-compute-400214/web-container --platform managed --region europe-west2 --allow-unauthenticated
+gcloud run deploy web-container --image gcr.io/phd-compute-400214/web-container --platform managed --region europe-west2 --allow-unauthenticated
 # delete old revisions
 gcloud run revisions list --region europe-west2 --filter="status.conditions.type:Active AND status.conditions.status:'False'" --format='value(metadata.name)' | xargs -r -L1 gcloud run revisions delete --region europe-west2 --quiet
 # remove docker image
